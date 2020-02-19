9cafb936f720885607eda27414368ce34eeec0fc
Nolan Picini <nolanpic@gmail.com>
2020-02-19 13:09:59 -0800

    Rough in model functions and run migrations

---
+++ schemes/scheme-model.js
@@ -0,0 +1,38 @@
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    addStep,
+    update,
+    remove
+};
+
+function find() {
+
+}
+
+function findById(id) {
+    
+}
+
+function findSteps(id) {
+
+}
+
+function add(scheme) {
+
+}
+
+function addStep(step, schemeId) {
+
+}
+
+function update(id, updates) {
+
+}
+
+function remove(id) {
+
+}
